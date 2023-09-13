116be4a0973d51ca7be722f9f85655dc1065f29a
Olga Yepikhina <olga.yepikhina@gmail.com>
2023-09-13 08:57:35 +0200

    FInish 02

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,8 @@
 console.log('DOM');
+
+const firstElement = document.querySelector('.comments__item.comments__item--newest');
+console.log(firstElement);
+if(firstElement) {
+    const elementWithAttr = firstElement.querySelectorAll('[data-info]');
+console.log(elementWithAttr.length);
+};
--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,8 @@
 console.log('DOM');
+
+const linkList = document.querySelectorAll('a[data-url]');
+console.log(linkList);
+linkList.forEach (function (el) {
+    const attr = el.getAttribute('data-url');
+    el.setAttribute('href', attr);
+});
