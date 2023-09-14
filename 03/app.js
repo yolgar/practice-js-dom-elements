6d58c14189620f1c6d57485a3018a6312046cffd
Olga Yepikhina <olga.yepikhina@gmail.com>
2023-09-14 08:36:02 +0200

    FInish03'

--- 03/app.js
+++ 03/app.js
@@ -12,3 +12,20 @@ const buttonSettings = {
     },
     text: 'Click me!',
 }
+
+const newButtonParent = document.querySelector('.parent-for-button');
+const newButton = document.createElement('button');
+
+newButtonParent.appendChild(newButton);
+
+for(const key in buttonSettings.attr) {
+    const value = buttonSettings.attr[key];
+    newButton[key] = value;
+}
+
+for(const key in buttonSettings.css) {
+    const value = buttonSettings.css[key];
+    newButton.style[key] = value;
+}
+
+newButton.innerText = buttonSettings.text;
