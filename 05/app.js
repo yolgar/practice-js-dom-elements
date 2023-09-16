27c4601e51c249cb88e047226d6e1712e7fd8fc2
Olga Yepikhina <olga.yepikhina@gmail.com>
2023-09-16 10:40:33 +0200

    Finish 05

--- 05/app.js
+++ 05/app.js
@@ -1,3 +1,31 @@
 console.log('DOM');
 
 const curr = document.querySelector('.js-curr');
+
+const newButton = document.createElement('button');
+newButton.innerText = 'usuń z koszyka';
+curr.parentElement.appendChild(newButton);
+
+
+const sblList = Array.from(curr.parentElement.children);
+sblList.forEach(function(item){
+    if(item !== curr){
+    item.classList.add('siblings');
+}
+})
+
+
+const artc = curr.parentElement.nextElementSibling;
+console.log(artc);
+artc.setAttribute('title', 'nextElementSibling');
+
+
+const lstAtr = artc.nextElementSibling;
+console.log(lstAtr);
+const btnEl = lstAtr.children[lstAtr.children.length -1];
+const pElement = document.createElement('p');
+lstAtr.insertBefore(pElement,btnEl);
+
+
+const newArtc = curr.parentElement.cloneNode(true);
+curr.parentElement.parentElement.insertBefore(newArtc, curr.parentElement);
